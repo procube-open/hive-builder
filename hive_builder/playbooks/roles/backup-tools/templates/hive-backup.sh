8dee93d4beb2a805721adf07e107433239e0912e
nnatoyaak <nnatoyaak@gmail.com>
2021-09-06 19:34:53 +0000

    バックアップの対象名に"hive-"を追加

--- hive_builder/playbooks/roles/backup-tools/templates/hive-backup.sh
+++ hive_builder/playbooks/roles/backup-tools/templates/hive-backup.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-LOGGER=${0##*/}
+GGER=${0##*/}
 
 function message () {
   echo "INFO: $LOGGER:" $*
@@ -12,7 +11,7 @@ function error () {
 
 mode=backup
 services=$(/bin/ls /var/lib/hive-backup.d)
-services="zabbix registry ${services}"
+services="hive-zabbix hive-registry ${services}"
 targets=$services
 
 usage_exit() {
@@ -51,90 +50,94 @@ if [ $# -ge $OPTIND ]; then
   usage_exit
 fi
 
-backupdir="{{ hive_home_dir }}/backup"
+backupdir="/home/admin/backup"
 mkdir -p "$backupdir"
 cd "$backupdir"
 
 for service in $targets; do
-  if [ ${service} = "zabbix" ]; then
-    function backup_zabbix() {
-      message "ENTER SERVICE zabbix for BACKUP"
-      # for backup zabbix
-      backup_file="backup-zabbix-$(date +%Y%m%d%H%M%S).sql.gz"
-      message "START backup zabbix into $backup_file":
+  if [ ${service} = "hive-zabbix" ]; then
+    function backup_hive-zabbix() {
+      message "ENTER SERVICE hive-zabbix for BACKUP"
+      # for backup hive-zabbix
+      backup_file="backup-hive-zabbix-$(date +%Y%m%d%H%M%S).sql.gz"
+      message "START backup hive-zabbix into $backup_file":
       if (cd /home/admin/zabbix/;docker-compose exec -T zabbix-db sh -c 'mysqldump -u zabbix -pzabbix zabbix | gzip') > "$backup_file"; then
-        message "END backup for zabbix into $backup_file"
-        message "LINK backup-zabbix-latest.sql.gz to $backup_file"
-        rm -f "backup-zabbix-latest.sql.gz"
-        ln -s "$backup_file" "backup-zabbix-latest.sql.gz"
+        message "END backup for hive-zabbix into $backup_file"
+        message "LINK backup-hive-zabbix-latest.sql.gz to $backup_file"
+        rm -f "backup-hive-zabbix-latest.sql.gz"
+        ln -s "$backup_file" "backup-hive-zabbix-latest.sql.gz"
       else
-        message "FAIL backup for zabbix"
+        message "FAIL backup for hive-zabbix"
       fi
-      message "Clean up old files than 10 like backup-zabbix-*.sql.gz"
-      find ./ -name "backup-zabbix-*.sql.gz" -mtime +10 -type f | xargs rm -f
-      message "LEAVE SERVICE zabbix for BACKUP"
+      message "Clean up old files than 10 like backup-hive-zabbix-*.sql.gz"
+      find ./ -name "backup-hive-zabbix-*.sql.gz" -mtime +10 -type f | xargs rm -f
+      message "LEAVE SERVICE hive-zabbix for BACKUP"
     }
-    function restore_zabbix() {
-      message "ENTER SERVICE zabbix for RESTORE"
-      # for restore zabbix
-      backup_file="backup-zabbix-latest.sql.gz"
+    function restore_hive-zabbix() {
+      message "ENTER SERVICE hive-zabbix for RESTORE"
+      # for restore hive-zabbix
+      backup_file="backup-hive-zabbix-latest.sql.gz"
       if [ -r $backup_file ]; then
-        message "START restore for zabbix from $backup_file"
+        message "START restore for hive-zabbix from $backup_file"
         cp "$backup_file" /home/admin/zabbix/"$backup_file";
         cd /home/admin/zabbix;
         docker cp "$backup_file" $(docker-compose ps -q zabbix-db):/root/today.sql.gz
         if docker-compose exec -T zabbix-db sh -c 'zcat /root/today.sql.gz | mysql -B -u zabbix -pzabbix -D zabbix'; then
-          message "END restore for zabbix"
+          message "END restore for hive-zabbix"
         else
-          message "FAIL restore for zabbix"
+          message "FAIL restore for hive-zabbix"
         fi
       else
-        message "SKIP restore for zabbix which does not have backup-zabbix-latest"
+        message "SKIP restore for hive-zabbix which does not have backup-hive-zabbix-latest"
       fi
-      message "LEAVE SERVICE zabbix for RESTORE"
+      message "LEAVE SERVICE hive-zabbix for RESTORE"
       }
   ${mode}_${service}
-  elif [ ${service} = "registry" ]; then
-    function backup_registry() {
-      message "ENTER SERVICE registry for BACKUP"
-      # for backup registry
-      backup_file="backup-registry-$(date +%Y%m%d%H%M%S).tar.gz"
-      message "START backup registry into $backup_file":
+  cd "$backupdir"
+  elif [ ${service} = "hive-registry" ]; then
+    function backup_hive-registry() {
+      message "ENTER SERVICE hive-registry for BACKUP"
+      # for backup hive-registry
+      backup_file="backup-hive-registry-$(date +%Y%m%d%H%M%S).tar.gz"
+      message "START backup hive-registry into $backup_file":
       if (cd /home/admin/registry/;docker-compose exec -T registry_server sh -c 'cd /var/lib/registry; tar czf - $(find . -maxdepth 1 -not -name .)') > "$backup_file"; then
-        message "END backup for registry into $backup_file"
-        message "LINK backup-registry-latest.tar.gz to $backup_file"
-        rm -f "backup-registry-latest.tar.gz"
-        ln -s "$backup_file" "backup-registry-latest.tar.gz"
+        message "END backup for hive-registry into $backup_file"
+        message "LINK backup-hive-registry-latest.tar.gz to $backup_file"
+        rm -f "backup-hive-registry-latest.tar.gz"
+        ln -s "$backup_file" "backup-hive-registry-latest.tar.gz"
       else
-        message "FAIL backup for registry"
+        message "FAIL backup for hive-registry"
       fi
-      message "Clean up old files than 10 like backup-registry-*.tar.gz"
-      find ./ -name "backup-registry-*.tar.gz" -mtime +10 -type f | xargs rm -f
-      message "LEAVE SERVICE registry for BACKUP"
+      message "Clean up old files than 10 like backup-hive-registry-*.tar.gz"
+      find ./ -name "backup-hive-registry-*.tar.gz" -mtime +10 -type f | xargs rm -f
+      message "LEAVE SERVICE hive-registry for BACKUP"
     }
-    function restore_registry() {
-      message "ENTER SERVICE registry for RESTORE"
-      # for restore registry
-      backup_file="backup-registry-latest.tar.gz"
+    function restore_hive-registry() {
+      message "ENTER SERVICE hive-registry for RESTORE"
+      # for restore hive-registry
+      backup_file="backup-hive-registry-latest.tar.gz"
       if [ -r $backup_file ]; then
-        message "START restore for registry from $backup_file"
+        message "START restore for hive-registry from $backup_file"
         cp "$backup_file" /home/admin/registry/"$backup_file";
         cd /home/admin/registry;
         docker cp "$backup_file" $(docker-compose ps -q registry_server):/root/today.tar.gz
         if docker-compose exec -T registry_server sh -c 'cd /var/lib/registry; rm -rf $(find . -maxdepth 1 -not -name .); tar xzf /root/today.tar.gz'; then
-          message "END restore for registry"
+          message "END restore for hive-registry"
         else
-          message "FAIL restore for registry"
+          message "FAIL restore for hive-registry"
         fi
       else
-        message "SKIP restore for registry which does not have backup-registry-latest"
+        message "SKIP restore for hive-registry which does not have backup-hive-registry-latest"
       fi
-      message "LEAVE SERVICE registry for RESTORE"
+      message "LEAVE SERVICE hive-registry for RESTORE"
       }
   ${mode}_${service}
+  cd "$backupdir"
   else
   source /var/lib/hive-backup.d/${service}
   ${mode}_${service}
+  cd "$backupdir"
   fi
 done
+
 
